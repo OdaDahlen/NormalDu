8aedc48446902b07cdf5b4c1c8eb472af229aa18
odahub <oda.dahlen@gmail.com>
2018-03-25 21:54:20 +0200

    "First commit"

---
+++ app/src/main/java/com/example/odapro/normalduer/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.odapro.normalduer;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // The buttons
+        Button mBkropp;
+        Button mButseendet;
+        Button mBsosialt;
+        Button mBforeldre;
+        Button mBpersonlighet;
+        Button mBgenerelt;
+        Button mBtilfeldige;
+        Button loginbutton;
+
+        mBkropp = (Button) findViewById(R.id.mBkropp);
+        mBkropp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DisplayKropp.class));
+            }
+        });
+
+
+
+
+        //Data endata = new Data(1, "navnet", "adressen");
+        //DBHandler enhandler = new DBHandler(this);
+
+        Log.d("hei du", "better be message");
+        //System.out.println(enhandler.getAllData());
+        Log.d("there you are", "her er en message");
+    }
+}
